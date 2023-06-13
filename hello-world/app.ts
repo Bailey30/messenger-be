4e06071b524239269a8c1f8cc7fa2af55d636f56
Alex <baileymk@hotmail.com>
2023-06-13 13:35:51 +0100

    Changed return message

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -18,7 +18,7 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
         return {
             statusCode: 200,
             body: JSON.stringify({
-                message: 'hello friend',
+                message: 'i am using canary deployments',
             }),
         };
     } catch (err) {
