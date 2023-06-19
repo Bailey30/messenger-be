7f3e6813ce6e6ae61633f5c55431ef8537c53547
Alex <baileymk@hotmail.com>
2023-06-19 17:22:01 +0100

    compiled to js

--- src/lambda/postConfirmation/app.js
+++ src/lambda/postConfirmation/app.js
@@ -1,25 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.postConfirmationHandler = void 0;
-const postConfirmationHandler = async (event) => {
+const postConfirmationHandler = async (event, context, callback) => {
     console.log('EVENT', event);
-    console.info('EVENT\n' + JSON.stringify(event, null, 2));
-    try {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'websocket connected',
-            }),
-        };
-    }
-    catch (err) {
-        console.log(err);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: 'some error happened',
-            }),
-        };
-    }
+    console.info('EVENT info\n' + JSON.stringify(event, null, 2));
+    return event;
 };
 exports.postConfirmationHandler = postConfirmationHandler;
