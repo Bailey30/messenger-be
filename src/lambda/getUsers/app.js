e1c4398ce6aaf44be0ea65d4f05f97f9db74e31f
Alex <baileymk@hotmail.com>
2023-07-03 16:14:54 +0100

    get users function

--- src/lambda/getUsers/app.js
+++ src/lambda/getUsers/app.js
@@ -2,8 +2,29 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getUsersHandler = void 0;
 const client_dynamodb_1 = require("@aws-sdk/client-dynamodb");
+const lib_dynamodb_1 = require("@aws-sdk/lib-dynamodb");
 const client = new client_dynamodb_1.DynamoDBClient({ region: 'eu-west-2' });
-const getUsersHandler = (event) => {
-    //
+const dynamo = lib_dynamodb_1.DynamoDBDocumentClient.from(client);
+const getUsersHandler = async (event, context, callback) => {
+    try {
+        const params = {
+            TableName: process.env.USERS_TABLE_NAME,
+            ExpressionAttributeNames: { '#onlineStatus': 'onlineStatus' },
+            ExpressionAttributeValues: { ':onlineStatus': { S: 'online' } },
+            FilterExpression: '#onlineStatus = :onlineStatus',
+        };
+        const onlineUsers = await dynamo.send(new client_dynamodb_1.ScanCommand(params));
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(onlineUsers),
+        });
+    }
+    catch (error) {
+        console.log(error);
+        callback(null, {
+            statusCode: 500,
+            body: JSON.stringify(error),
+        });
+    }
 };
 exports.getUsersHandler = getUsersHandler;
--- src/lambda/getUsers/app.ts
+++ src/lambda/getUsers/app.ts
@@ -1,7 +1,28 @@
-import { DynamoDBClient, BatchExecuteStatementCommand } from '@aws-sdk/client-dynamodb';
+import { DynamoDBClient, BatchExecuteStatementCommand, ScanCommand } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 
 const client = new DynamoDBClient({ region: 'eu-west-2' });
+const dynamo = DynamoDBDocumentClient.from(client);
+export const getUsersHandler = async (event: any, context: any, callback: any) => {
+    try {
+        const params = {
+            TableName: process.env.USERS_TABLE_NAME,
+            ExpressionAttributeNames: { '#onlineStatus': 'onlineStatus' },
+            ExpressionAttributeValues: { ':onlineStatus': { S: 'online' } },
+            FilterExpression: '#onlineStatus = :onlineStatus',
+        };
 
-export const getUsersHandler = (event: any) => {
-    //
+        const onlineUsers = await dynamo.send(new ScanCommand(params));
+
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(onlineUsers),
+        });
+    } catch (error) {
+        console.log(error);
+        callback(null, {
+            statusCode: 500,
+            body: JSON.stringify(error),
+        });
+    }
 };
