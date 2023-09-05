2b66e86f0b43c39bd5ff3119f278d4d3a50b2c32
Alex <baileymk@hotmail.com>
2023-09-05 11:15:13 +0100

    changed aws sdk

--- src/lambda/sendMessage/app.js
+++ src/lambda/sendMessage/app.js
@@ -5,46 +5,57 @@ const client_dynamodb_1 = require("@aws-sdk/client-dynamodb");
 const lib_dynamodb_1 = require("@aws-sdk/lib-dynamodb");
 const client = new client_dynamodb_1.DynamoDBClient({ region: 'eu-west-2' });
 const dynamo = lib_dynamodb_1.DynamoDBDocumentClient.from(client);
-const AWS = require('aws-sdk');
 const sendMessageHandler = async (event, context, callback) => {
-    console.log({ event });
-    // Retrieve connectionIds from the connections table
-    const params = {
-        TableName: process.env.CONNECTIONS_TABLE_NAME,
-        ProjectionExpression: 'connectionId',
-    };
-    const scanResponse = await dynamo.send(new client_dynamodb_1.ScanCommand(params));
-    // Get the API endpoint
-    const endpoint = event.requestContext.domainName + '/' + event.requestContext.stage;
-    console.log({ endpoint });
-    // Create an instance of the API Gateway Management API
-    const apigwManagementApi = new AWS.ApiGatewayManagementApi({
-        apiVersion: '2018-11-29',
-        endpoint: event.requestContext.domainName + '/' + event.requestContext.stage,
-    });
-    // Send messages to each connectionId retrieved from the table
-    const postCalls = scanResponse?.Items?.map(async ({ connectionId }) => {
-        try {
-            await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: 'hello' }).promise();
-        }
-        catch (e) {
-            // If the connection is stale, delete it from the table
-            if (e.statusCode === 410) {
-                console.log(`Found stale connection, deleting ${connectionId}`);
-                await dynamo.send(new lib_dynamodb_1.DeleteCommand({ TableName: process.env.CONNECTIONS_TABLE_NAME, Key: { connectionId } }));
-                throw e;
-            }
-        }
-    });
     try {
-        // Wait for all post calls to complete
-        postCalls && (await Promise.all(postCalls));
+        console.log({ event });
+        // Retrieve connectionIds from the connections table
+        // const params = {
+        //     TableName: process.env.CONNECTIONS_TABLE_NAME,
+        //     IndexName: 'cognitoid-index', // Name of the global secondary index
+        //     KeyConditionExpression: 'cognitoid = :cognitoId',
+        //     ExpressionAttributeValues: {
+        //         ':cognitoId': cognitoId,
+        //     },
+        // };
+        // try {
+        //     const scanResponse = await dynamo.send(new ScanCommand(params));
+        //     // Check if any items were found
+        //     if (scanResponse.Items && scanResponse.Items.length > 0) {
+        //         // Extract the connection ID from the first item (assuming unique cognito IDs)
+        //         const connectionId = scanResponse.Items[0].connectionId;
+        //         console.log('Connection ID:', connectionId);
+        //     } else {
+        //         console.log('No matching records found for the cognito ID:', cognitoId);
+        //     }
+        // } catch (error) {
+        //     console.error('Error querying DynamoDB:', error);
+        // }
+        // // Get the API endpoint
+        // const endpoint = event.requestContext.domainName + '/' + event.requestContext.stage;
+        // console.log({ endpoint });
+        // // Create an instance of the API Gateway Management API
+        // const APIGWClient = new ApiGatewayManagementApiClient({
+        //     region: 'eu-west-2',
+        //     endpoint: event.requestContext.domainName + '/' + event.requestContext.stage,
+        // });
+        // // Send messages to each connectionId retrieved from the table
+        // try {
+        //     await APIGWClient.send(new PostToConnectionCommand({ ConnectionId: connectionId, Data: 'hello' }));
+        // } catch (e: any) {
+        //     // If the connection is stale, delete it from the table
+        //     if (e.statusCode === 410) {
+        //         console.log(`Found stale connection, deleting ${connectionId}`);
+        //         await dynamo.send(
+        //             new DeleteCommand({ TableName: process.env.CONNECTIONS_TABLE_NAME, Key: { connectionId } }),
+        //         );
+        //         throw e;
+        //     }
+        // }
+        // Return a success response
+        return { statusCode: 200, body: 'Data sent.' };
     }
-    catch (e) {
-        // Return an error response if there was an exception
-        return { statusCode: 500, body: e.stack };
+    catch (error) {
+        return { statusCode: 500, body: 'error sending message' };
     }
-    // Return a success response
-    return { statusCode: 200, body: 'Data sent.' };
 };
 exports.sendMessageHandler = sendMessageHandler;
--- src/lambda/sendMessage/app.ts
+++ src/lambda/sendMessage/app.ts
@@ -4,48 +4,63 @@ import { DeleteCommand, DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 const client = new DynamoDBClient({ region: 'eu-west-2' });
 const dynamo = DynamoDBDocumentClient.from(client);
 
-const AWS = require('aws-sdk');
+import { PostToConnectionCommand, ApiGatewayManagementApiClient } from '@aws-sdk/client-apigatewaymanagementapi';
+
 export const sendMessageHandler = async (event: any, context: any, callback: any) => {
-    console.log({ event });
-    // Retrieve connectionIds from the connections table
-    const params = {
-        TableName: process.env.CONNECTIONS_TABLE_NAME,
-        ProjectionExpression: 'connectionId',
-    };
-    const scanResponse = await dynamo.send(new ScanCommand(params));
-
-    // Get the API endpoint
-    const endpoint = event.requestContext.domainName + '/' + event.requestContext.stage;
-    console.log({ endpoint });
-
-    // Create an instance of the API Gateway Management API
-    const apigwManagementApi = new AWS.ApiGatewayManagementApi({
-        apiVersion: '2018-11-29',
-        endpoint: event.requestContext.domainName + '/' + event.requestContext.stage,
-    });
-
-    // Send messages to each connectionId retrieved from the table
-    const postCalls = scanResponse?.Items?.map(async ({ connectionId }) => {
-        try {
-            await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: 'hello' }).promise();
-        } catch (e: any) {
-            // If the connection is stale, delete it from the table
-            if (e.statusCode === 410) {
-                console.log(`Found stale connection, deleting ${connectionId}`);
-                await dynamo.send(
-                    new DeleteCommand({ TableName: process.env.CONNECTIONS_TABLE_NAME, Key: { connectionId } }),
-                );
-                throw e;
-            }
-        }
-    });
     try {
-        // Wait for all post calls to complete
-        postCalls && (await Promise.all(postCalls));
-    } catch (e: any) {
-        // Return an error response if there was an exception
-        return { statusCode: 500, body: e.stack };
+        console.log({ event });
+        // Retrieve connectionIds from the connections table
+        // const params = {
+        //     TableName: process.env.CONNECTIONS_TABLE_NAME,
+        //     IndexName: 'cognitoid-index', // Name of the global secondary index
+        //     KeyConditionExpression: 'cognitoid = :cognitoId',
+        //     ExpressionAttributeValues: {
+        //         ':cognitoId': cognitoId,
+        //     },
+        // };
+
+        // try {
+        //     const scanResponse = await dynamo.send(new ScanCommand(params));
+        //     // Check if any items were found
+        //     if (scanResponse.Items && scanResponse.Items.length > 0) {
+        //         // Extract the connection ID from the first item (assuming unique cognito IDs)
+        //         const connectionId = scanResponse.Items[0].connectionId;
+        //         console.log('Connection ID:', connectionId);
+        //     } else {
+        //         console.log('No matching records found for the cognito ID:', cognitoId);
+        //     }
+        // } catch (error) {
+        //     console.error('Error querying DynamoDB:', error);
+        // }
+
+        // // Get the API endpoint
+        // const endpoint = event.requestContext.domainName + '/' + event.requestContext.stage;
+        // console.log({ endpoint });
+
+        // // Create an instance of the API Gateway Management API
+
+        // const APIGWClient = new ApiGatewayManagementApiClient({
+        //     region: 'eu-west-2',
+        //     endpoint: event.requestContext.domainName + '/' + event.requestContext.stage,
+        // });
+
+        // // Send messages to each connectionId retrieved from the table
+        // try {
+        //     await APIGWClient.send(new PostToConnectionCommand({ ConnectionId: connectionId, Data: 'hello' }));
+        // } catch (e: any) {
+        //     // If the connection is stale, delete it from the table
+        //     if (e.statusCode === 410) {
+        //         console.log(`Found stale connection, deleting ${connectionId}`);
+        //         await dynamo.send(
+        //             new DeleteCommand({ TableName: process.env.CONNECTIONS_TABLE_NAME, Key: { connectionId } }),
+        //         );
+        //         throw e;
+        //     }
+        // }
+
+        // Return a success response
+        return { statusCode: 200, body: 'Data sent.' };
+    } catch (error) {
+        return { statusCode: 500, body: 'error sending message' };
     }
-    // Return a success response
-    return { statusCode: 200, body: 'Data sent.' };
 };
